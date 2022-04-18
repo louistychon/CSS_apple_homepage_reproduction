33c856ff25138908ed2ad6e74852885406b0380e
louistychon <louis.tychon1@gmail.com>
2022-04-18 14:26:32 +0200

    responsive menu & lists footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const menu = document.querySelector(".menu-long");
 const menuItems = document.querySelectorAll(".nav-item");
-const hamburger= document.querySelector(".hamburger");
+const hamburger = document.querySelector(".hamburger");
 // const closeIcon= document.querySelector(".closeIcon");
 // const menuIcon = document.querySelector(".menuIcon");
 
@@ -20,11 +20,30 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 
-menuItems.forEach( 
-  function(menuItem) { 
+menuItems.forEach(
+  function (menuItem) {
     menuItem.addEventListener("click", toggleMenu);
   }
 )
 
-
 // bottom menu
+let widths = [0, 768, 1000];
+const ulFooter = document.getElementsByClassName("list-footer");
+let open = false;
+
+function showUl() {
+  if(open === false && window.innerWidth>=widths[0] && window.innerWidth<widths[1]){
+  for (var x = 0; x < ulFooter.length; x++) {
+    ulFooter[x].style.display = "block";
+
+  }
+  open = true;
+}
+  else if (open === true && window.innerWidth>=widths[0] && window.innerWidth<widths[1]) {
+    for (var x = 0; x < ulFooter.length; x++) {
+      ulFooter[x].style.display = "none";
+    }
+    open = false;
+  }
+
+}
